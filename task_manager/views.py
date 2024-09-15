14518dff931dfcb578ac11e4ec49804c74e9eb29
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2024-09-15 15:11:54 +0300

    add views.py

---
+++ task_manager/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views import View
+from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+
+from .forms import LoginUserForm
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        messages_ = messages.get_messages(request)
+
+        return render(
+            request,
+            'index.html',
+            context={
+                'messages': messages_
+            }
+        )
+
+
+class LoginUser(SuccessMessageMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'login.html'
+    success_message = _('You are logged in')
+
+
+class LogoutUser(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.info(request, _("You are logged out."))
+        return super().dispatch(request, *args, **kwargs)
