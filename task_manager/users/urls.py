d4a1e4350ed8e3ad7f6c0e897961dd932617737f
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2024-09-15 16:22:57 +0300

    add urls.py

---
+++ task_manager/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.IndexIndex.as_view(), name='index_users'),
+    path('create/', views.CreateUser.as_view(), name='create_user'),
+    path('<int:id>/update/', views.UpdateUser.as_view(), name='update_user'),
+    path('<int:id>/delete/', views.DeleteUser.as_view(), name='delete_user'),
+]
