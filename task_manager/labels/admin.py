2285858dfcc73cc1fb75e0ec9f0968ea706c0d57
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2024-09-15 19:52:59 +0300

    update models

--- task_manager/labels/admin.py
+++ task_manager/labels/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Label
+
+
+@admin.register(Label)
+class LabelsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'timestamp')
+    search_fields = ['name']
+    list_filter = (('timestamp', DateFieldListFilter),)
--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name=_('Name'))
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
