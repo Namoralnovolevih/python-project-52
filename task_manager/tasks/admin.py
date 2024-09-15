14462f244690352f7207a07b213d71c75caa568a
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2024-09-15 15:42:19 +0300

    update admin tasks

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter, RelatedFieldListFilter
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status', 'author', 'executor', 'timestamp')
+    search_fields = ['name']
+    list_filter = (('timestamp', DateFieldListFilter),
+                   ('status', RelatedFieldListFilter),
+                   ('author', RelatedFieldListFilter),
+                   ('executor', RelatedFieldListFilter),
+                   ('labels', RelatedFieldListFilter),
+                   )
