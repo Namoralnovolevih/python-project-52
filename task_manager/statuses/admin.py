e8938fb4aa00634ece96d78b563c6a66bce028a5
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2024-09-15 19:16:28 +0300

    update statuses admin

--- task_manager/statuses/admin.py
+++ task_manager/statuses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Status
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'timestamp')
+    search_fields = ['name']
+    list_filter = (('timestamp', DateFieldListFilter),)
